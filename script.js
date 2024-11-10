c8471c686ef58e81cff61af0eb1883c01c53bf57
danielwmartin1 <danielwmartin1@gmail.com>
2024-11-10 03:41:04 -0500

    Add contact page with email link and update styles for improved usability

--- script.js
+++ script.js
@@ -24,4 +24,11 @@ $(document).ready(function() {
 
     $(window).on('resize', handleResize);
     handleResize();
+
+    $('.about-me-email').on('click', function(e) {
+        e.preventDefault();
+        const email = 'danielwmartin1@gmail.com';
+        window.location.href = `mailto:${email}`;
+    });
 });
+
